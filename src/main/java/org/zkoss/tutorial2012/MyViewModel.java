ba7737c834341fa0840ea8224a4e7db46bb23ea5
Dennis <atticcat@gmail.com>
2012-12-17 16:08:43 +0800

    init

---
+++ src/main/java/org/zkoss/tutorial2012/MyViewModel.java
@@ -0,0 +1,17 @@
+package org.zkoss.tutorial2012;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+
+public class MyViewModel {
+
+	@Init
+	public void init() {
+
+	}
+
+	@Command
+	public void cmd() {
+
+	}
+}
