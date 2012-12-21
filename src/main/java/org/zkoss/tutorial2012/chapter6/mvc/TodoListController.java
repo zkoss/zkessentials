ca5e8f6ea42eac1e1a695b0327cd96db23c6226d
Dennis <atticcat@gmail.com>
2012-12-21 16:30:57 +0800

    chapter6

---
+++ src/main/java/org/zkoss/tutorial2012/chapter6/mvc/TodoListController.java
@@ -0,0 +1,9 @@
+package org.zkoss.tutorial2012.chapter6.mvc;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+
+public class TodoListController extends SelectorComposer<Component>{
+	private static final long serialVersionUID = 1L;
+
+}
