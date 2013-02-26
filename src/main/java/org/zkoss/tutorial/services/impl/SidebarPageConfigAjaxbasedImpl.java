e67f805b093cdd11f75845b32bde2c6fd6aa7ee2
Hawk Chen <hawkchen@potix.com>
2013-02-26 17:16:11 +0800

    pagebase -> pagebased singledesktop -> ajaxbased

--- src/main/java/org/zkoss/tutorial/services/impl/SidebarPageConfigAjaxbasedImpl.java
+++ src/main/java/org/zkoss/tutorial/services/impl/SidebarPageConfigAjaxbasedImpl.java
@@ -19,20 +19,20 @@ import org.springframework.stereotype.Component;
 import org.zkoss.tutorial.services.SidebarPage;
 import org.zkoss.tutorial.services.SidebarPageConfig;
 
-@Component("sidebarPageConfigPagebase")
+@Component("sidebarPageConfigSingleDesktop")
 @Scope(value="request",proxyMode=ScopedProxyMode.TARGET_CLASS)
-public class SidebarPageConfigPagebaseImpl implements SidebarPageConfig{
+public class SidebarPageConfigAjaxbasedImpl implements SidebarPageConfig{
 	
 	HashMap<String,SidebarPage> pageMap = new LinkedHashMap<String,SidebarPage>();
-	public SidebarPageConfigPagebaseImpl(){		
+	public SidebarPageConfigAjaxbasedImpl(){		
 		pageMap.put("zk",new SidebarPage("zk","www.zkoss.org","/imgs/site.png","http://www.zkoss.org/"));
 		pageMap.put("demo",new SidebarPage("demo","ZK Demo","/imgs/demo.png","http://www.zkoss.org/zkdemo"));
 		pageMap.put("devref",new SidebarPage("devref","ZK Developer Reference","/imgs/doc.png","http://books.zkoss.org/wiki/ZK_Developer's_Reference"));
 		
-		pageMap.put("fn1",new SidebarPage("fn1","Profile (MVC)","/imgs/fn.png","/chapter7/pagebase/index-profile-mvc.zul"));
-		pageMap.put("fn2",new SidebarPage("fn2","Profile (MVVM)","/imgs/fn.png","/chapter7/pagebase/index-profile-mvvm.zul"));
-		pageMap.put("fn3",new SidebarPage("fn3","Todo List (MVC)","/imgs/fn.png","/chapter7/pagebase/index-todolist-mvc.zul"));
-		pageMap.put("fn4",new SidebarPage("fn4","Todo List (MVVM)","/imgs/fn.png","/chapter7/pagebase/index-todolist-mvvm.zul"));
+		pageMap.put("fn1",new SidebarPage("fn1","Profile (MVC)","/imgs/fn.png","/chapter5/profile-mvc.zul"));
+		pageMap.put("fn2",new SidebarPage("fn2","Profile (MVVM)","/imgs/fn.png","/chapter5/profile-mvvm.zul"));
+		pageMap.put("fn3",new SidebarPage("fn3","Todo List (MVC)","/imgs/fn.png","/chapter6/todolist-mvc.zul"));
+		pageMap.put("fn4",new SidebarPage("fn4","Todo List (MVVM)","/imgs/fn.png","/chapter6/todolist-mvvm.zul"));
 	}
 	
 	public List<SidebarPage> getPages(){
